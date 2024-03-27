3be24bc5b56aa32dfb5004e32f1563aa5b1fc41d
Sam <lh22899@bristol.ac.uk>
2024-03-27 20:02:24 +0000

    Edited code to now have two separate plots

--- Stochastic_SIR.py
+++ Stochastic_SIR.py
@@ -17,6 +17,7 @@ gamma = 0.01    # Recovery Rate (1/gamma = Recovery period)
     This is the expected number of new infections from a single infected person.
 """
 
+    # Gillespie algorithm
 while t[-1] < tend and (S[-1] + I[-1] >= 1):
 
     N = S[-1] + I[-1] + R[-1]
@@ -46,51 +47,62 @@ while t[-1] < tend and (S[-1] + I[-1] >= 1):
             R.append(R[-1] + 1)    
 
 
-f,(ax1,ax2,ax3) = plt.subplots(3)
+figure1,(ax1,ax2,ax3) = plt.subplots(3) # Plotting Gillespie ODE in figure 1:
 
-line1, = ax1.plot(t,S) # S
-line2, = ax2.plot(t,I) # I
-line3, = ax3.plot(t,R) # R
+line1 = ax1.plot(t,S) # S
+line2 = ax2.plot(t,I) # I
+line3 = ax3.plot(t,R) # R
 
-
-ax1.set_ylabel("S")
-ax2.set_ylabel("I")
-ax3.set_ylabel("R")
+ax1.set_ylabel("Susceptible")
+ax2.set_ylabel("Infected")
+ax3.set_ylabel("Recovered")
 ax3.set_xlabel("Time (arbitrary units)")
 plt.suptitle("Stochastic SIR Model")
 
+    # Overlapping normal ODE and Gillespie algorithm ODE:
+
+figure2,(ax1,ax2,ax3) = plt.subplots(3)
+
+# First plotting Gillespie ODE (again but now on figure 2):
+line1 = ax1.plot(t,S) # S
+line2 = ax2.plot(t,I) # I
+line3 = ax3.plot(t,R) # R
+
+# Standard (deterministic) SIR ODE:
+t = np.linspace(0,tend, num=1000)
+params = [beta,gamma]
+y0 = [100, 1, 0]
+
+
+def sim(variables, t, params):
+
+    S = variables[0]
+    I = variables[1]
+    R = variables[2]
+
+    N = S + I + R
+
+    beta = params[0]
+    gamma = params[1]
+
+    dSdt = -beta * I * S / N
+    dIdt = beta * I * S / N - gamma * I
+    dRdt = gamma * I
+
+    return([dSdt, dIdt, dRdt])
+
+
+y = odeint(sim, y0, t, args=(params,))
+
+# Now plotting standard ODE results (over Gillespie ODE in figure 2):
+line1 = ax1.plot(t,y[:,0]) # S
+line2 = ax2.plot(t,y[:,1]) # I
+line3 = ax3.plot(t,y[:,2]) # R
+
+ax1.set_ylabel("Susceptible")
+ax2.set_ylabel("Infected")
+ax3.set_ylabel("Recovered")
+ax3.set_xlabel("Time (arbitrary units)")
+plt.suptitle("Deterministic vs Stochastic SIR Model")
+
 plt.show()
-
-# plt.suptitle("Deterministic vs Stochastic SIR Model")
-# Standard (deterministic) SIR Model (ODE):
-
-# t = np.linspace(0,tend, num=1000)
-# params = [beta,gamma]
-# y0 = [100, 1, 0]
-
-
-# def sim(variables, t, params):
-
-#     S = variables[0]
-#     I = variables[1]
-#     R = variables[2]
-
-#     N = S + I + R
-
-#     beta = params[0]
-#     gamma = params[1]
-
-#     dSdt = -beta * I * S / N
-#     dIdt = beta * I * S / N - gamma * I
-#     dRdt = gamma * I
-
-#     return([dSdt, dIdt, dRdt])
-
-
-# y = odeint(sim, y0, t, args=(params,))
-
-# line1, = ax1.plot(t,y[:,0]) # S
-# line2, = ax2.plot(t,y[:,1]) # I
-# line3, = ax3.plot(t,y[:,2]) # R
-
-# plt.show()
